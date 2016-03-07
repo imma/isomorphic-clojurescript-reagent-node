8ca6d8caac7601bb3b06a11ebe7d6bc1366ce7e2
Tom Bombadil <amanibhavam@destructuring.org>
2016-03-06 17:27:32 -0800

    add tictactoe game stubs

--- src/demo/core.cljs
+++ src/demo/core.cljs
@@ -1,7 +1,8 @@
 (ns demo.core
   (:require [reagent.core :as reagent :refer [atom]]
             [secretary.core :as secretary :refer-macros [defroute]]
-            [memtest.core :as memtest]))
+            [memtest.core :as memtest]
+            [tictactoe.core :as tictactoe]))
 
 (def current-page (atom nil))
 
@@ -10,8 +11,13 @@
     [:div 
       [:a {:href "/"} "Game Menu:"]
       [:span {:style {:padding "5px"}}]
+
       [:a {:href "/memtest"} "Memtest"]
-      [:span {:style {:padding "5px"}}]]))
+      [:span {:style {:padding "5px"}}]
+
+      [:a {:href "/tictactoe"} "Tic Tac Toe"]
+      [:span {:style {:padding "5px"}}]
+      ]))
 
 (defn app-view []
   [:div [@current-page]])
@@ -28,6 +34,11 @@
   (.log js/console "memtest/memtest-page")
   (memtest/new-game)
   (reset! current-page memtest/memtest-page))
+
+(defroute "/tictactoe" []
+  (.log js/console "tictactoe/tictactoe-page")
+  (tictactoe/new-game)
+  (reset! current-page tictactoe/tictactoe-page))
 
 ; the server side doesn't have history, so we want to make sure current-page is populated
 (reset! current-page home-page)
