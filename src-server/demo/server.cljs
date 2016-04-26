0ee0d8c8eee79bd02af83b8ca7564a924584d05f
Tom Bombadil <amanibhavam@destructuring.org>
2016-04-26 14:22:20 -0700

    reference app so it can be redefined on reload

--- src-server/demo/server.cljs
+++ src-server/demo/server.cljs
@@ -4,19 +4,21 @@
 
 (nodejs/enable-util-print!)
 
-(def express (nodejs/require "express"))
-(def serve-static (nodejs/require "serve-static"))
+(defonce express (nodejs/require "express"))
+(defonce serve-static (nodejs/require "serve-static"))
+(defonce http (nodejs/require "http"))
 
-(defn handle-request [req res]
- (.send res (tools/render-page (.-path req))))
+(def app (express))
+
+(. app (get "/" 
+            (fn [req res] (.send res (tools/render-page (.-path req))))))
+(. app (use (serve-static "resources/public/js/client")))
+(. app (use (serve-static "resources/public/static")))
 
 (defn -main []
-  (let [app (express)]
-    (.get app "/" handle-request)
-    (.use app (serve-static "resources/public/js/client"))
-    (.use app (serve-static "resources/public/css"))
-    (.use app (serve-static "resources/public/static"))
-    (.listen app 3002 (fn []
-                        (println "Server started on port 3002")))))
+  (doto 
+    (.createServer http #(app %1 %2))
+    (.listen 3002 (fn [] 
+                    (println "Server started on port 3002")))))
 
 (set! *main-cli-fn* -main)
