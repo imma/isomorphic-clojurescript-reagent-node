92af26d58b5194216ade777a4f824c57a81b16f0
Tom Bombadil <amanibhavam@destructuring.org>
2016-11-03 13:21:29 -0700

    making client/server similar

--- src-client/game/client.cljs
+++ src-client/game/client.cljs
@@ -5,12 +5,20 @@
             [game.core :as game])
   (:import goog.History))
 
-(enable-console-print!)
+(defn debug[]
+  (enable-console-print!))
 
-(reagent/render-component [game/app-view] (.getElementById js/document "app"))
+(defn setup[]
+  (reagent/render-component [game/app-view] (.getElementById js/document "app")))
 
 (def history
   (pushy/push-state! secretary/dispatch!
     (fn [x] (when (secretary/locate-route x) x))))
 
-(pushy/start! history)
+; main
+(defn -main[]
+  (pushy/start! history))
+
+(debug)
+(setup)
+(-main)
--- src-server/game/server.cljs
+++ src-server/game/server.cljs
@@ -2,26 +2,31 @@
   (:require [cljs.nodejs :as nodejs]
             [game.page :as page]))
 
-(nodejs/enable-util-print!)
+(defn debug[]
+  (nodejs/enable-util-print!))
 
-(defonce express (nodejs/require "express"))
-(defonce serve-static (nodejs/require "serve-static"))
-(defonce http (nodejs/require "http"))
+(defn setup[]
+  (defonce express (nodejs/require "express"))
+  (defonce serve-static (nodejs/require "serve-static"))
+  (defonce http (nodejs/require "http"))
 
-(def app (express))
+  (def app (express))
 
-(defn render-game[req res]
-  (.send res (page/render-page (.-path req))))
+  (defn render-game[req res]
+    (.send res (page/render-page (.-path req))))
 
-(. app (get "/" render-game))
-(. app (get "/memtest/" render-game))
-(. app (use (serve-static "resources/public/js/client")))
-(. app (use (serve-static "resources/public/static")))
+  (. app (get "/" render-game))
+  (. app (get "/memtest/" render-game))
+  (. app (use (serve-static "resources/public/js/client")))
+  (. app (use (serve-static "resources/public/static"))))
 
+; main
 (defn -main []
   (doto 
     (.createServer http #(app %1 %2))
     (.listen 3002 (fn [] 
                     (println "Server started on port 3002")))))
 
+(debug)
+(setup)
 (set! *main-cli-fn* -main)
